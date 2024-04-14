cf0e779d5718bc9ab6abfd35e15ab066b15bade2
eunji7122 <lej7122@gmail.com>
2024-04-14 23:26:29 +0900

    init project

---
+++ src/main/kotlin/com/study/devcommunityapi/DevcommunityapiApplication.kt
@@ -0,0 +1,11 @@
+package com.study.devcommunityapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DevcommunityapiApplication
+
+fun main(args: Array<String>) {
+    runApplication<DevcommunityapiApplication>(*args)
+}
---
+++ src/test/kotlin/com/study/devcommunityapi/DevcommunityapiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.study.devcommunityapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DevcommunityapiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
