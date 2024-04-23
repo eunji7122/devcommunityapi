3f14934ce9c167f090212932a9fbb98323e331d0
eunji7122 <lej7122@gmail.com>
2024-04-23 17:37:18 +0900

    Fix: Jwt 필터 두 번 호출 이슈 수정

--- src/main/kotlin/com/study/devcommunityapi/common/security/filter/JwtAuthenticationFilter.kt
+++ src/main/kotlin/com/study/devcommunityapi/common/security/filter/JwtAuthenticationFilter.kt
@@ -5,11 +5,9 @@ import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.security.core.context.SecurityContextHolder
-import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
 
 
-@Component
 class JwtAuthenticationFilter(
     private val jwtTokenProvider: JwtTokenProvider
 ): OncePerRequestFilter() {
